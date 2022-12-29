8d8804046f34cb410b8ee236f9639a790c5c7d3c
zubair-shabir <zubairshabir980@gmail.com>
2022-12-29 12:44:15 +0530

    bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBxf9HUrDRMIMiktaE8fo9ofimMPXl9UII",
+    authDomain: "github-searcher-41e81.firebaseapp.com",
+    projectId: "github-searcher-41e81",
+    storageBucket: "github-searcher-41e81.appspot.com",
+    messagingSenderId: "134933651794",
+    appId: "1:134933651794:web:051616591976cfff7f5a8b"
+  }
 };
